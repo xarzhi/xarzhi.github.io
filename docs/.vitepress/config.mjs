78fbf1adda06e689b3a6e6028932a813ca12238e
xarzhi <2235762265@qq.com>
2024-06-10 16:00:54 +0800

    up

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,78 +1,141 @@
 import { defineConfig } from 'vitepress'
 import navbar from './config/navbar'
 import sidebar from './config/sidebar'
-import timeline from 'vitepress-markdown-timeline' 
-export default defineConfig({
-	base: '/',
-	head: [
-		[
-			'meta',
-			{
-				name: 'referrer',
-				content: 'no-referrer',
-			},
+import timeline from 'vitepress-markdown-timeline'
+import { withPwa } from '@vite-pwa/vitepress'
+export default withPwa(
+	defineConfig({
+		base: '/',
+		head: [
+			[
+				'meta',
+				{
+					name: 'referrer',
+					content: 'no-referrer',
+				},
+			],
+			['link', { rel: 'icon', href: '/logo.png' }],
 		],
-		['link', { rel: 'icon', href: '/logo.png' }],
-	],
-	title: 'Fade away',
-	description: '花有重开日，人无在少年',
-	lastUpdated: true,
-	themeConfig: {
-		nav: navbar,
-		sidebar: sidebar,
-		outline: {
-			level: [2, 3, 4], // 显示2-4级标题
-			label: '当前页大纲', // 文字显示
-		},
-		logo: '/logo.png',
-		// 本地搜索
-		search: {
-			provider: 'local',
-			options: {
-				translations: {
-					button: {
-						buttonText: '搜索文档',
-						buttonAriaLabel: '搜索文档',
+		title: 'Fade away',
+		description: '花有重开日，人无在少年',
+		lastUpdated: true,
+		pwa: {
+			outDir: '.vitepress/dist', // 输出目录
+			registerType: 'autoUpdate', // 注册类型为自动更新
+			includeManifestIcons: false, // 不包含清单图标
+			manifest: {
+				id: '/', // 清单 ID
+				name: 'Fade away', // 应用名称
+				short_name: 'Fade away', // 应用的短名称
+				description: '个人笔记', // 应用的描述
+				theme_color: '#ffffff', // 主题颜色
+				icons: [],
+			},
+			workbox: {
+				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'], // 匹配需要缓存的文件类型
+				runtimeCaching: [
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i, // 匹配需要缓存的 Google 字体
+						handler: 'CacheFirst', // 缓存优先策略
+						options: {
+							cacheName: 'google-fonts-cache', // 缓存名称
+							expiration: {
+								maxEntries: 10, // 最大缓存条目数
+								maxAgeSeconds: 60 * 60 * 24 * 365, // 缓存有效期，365天
+							},
+							cacheableResponse: {
+								statuses: [0, 200], // 缓存的响应状态码
+							},
+						},
 					},
-					modal: {
-						noResultsText: '无法找到相关结果',
-						resetButtonTitle: '清除查询条件',
-						footer: {
-							selectText: '选择',
-							navigateText: '切换',
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i, // 匹配需要缓存的 Google 字体
+						handler: 'CacheFirst', // 缓存优先策略
+						options: {
+							cacheName: 'gstatic-fonts-cache', // 缓存名称
+							expiration: {
+								maxEntries: 10, // 最大缓存条目数
+								maxAgeSeconds: 60 * 60 * 24 * 365, // 缓存有效期，365天
+							},
+							cacheableResponse: {
+								statuses: [0, 200], // 缓存的响应状态码
+							},
+						},
+					},
+					{
+						urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i, // 匹配需要缓存的 jsdelivr 图片
+						handler: 'NetworkFirst', // 网络优先策略
+						options: {
+							cacheName: 'jsdelivr-images-cache', // 缓存名称
+							expiration: {
+								maxEntries: 10, // 最大缓存条目数
+								maxAgeSeconds: 60 * 60 * 24 * 7, // 缓存有效期，7天
+							},
+							cacheableResponse: {
+								statuses: [0, 200], // 缓存的响应状态码
+							},
+						},
+					},
+				],
+			},
+		},
+		themeConfig: {
+			nav: navbar,
+			sidebar: sidebar,
+			outline: {
+				level: [2, 3, 4], // 显示2-4级标题
+				label: '当前页大纲', // 文字显示
+			},
+			logo: '/logo.png',
+			// 本地搜索
+			search: {
+				provider: 'local',
+				options: {
+					translations: {
+						button: {
+							buttonText: '搜索文档',
+							buttonAriaLabel: '搜索文档',
+						},
+						modal: {
+							noResultsText: '无法找到相关结果',
+							resetButtonTitle: '清除查询条件',
+							footer: {
+								selectText: '选择',
+								navigateText: '切换',
+							},
 						},
 					},
 				},
 			},
-		},
-		//上次更新时间
-		lastUpdated: {
-			text: '上次更新时间',
-			formatOptions: {
-				dateStyle: 'full',
-				timeStyle: 'medium',
+			//上次更新时间
+			lastUpdated: {
+				text: '上次更新时间',
+				formatOptions: {
+					dateStyle: 'full',
+					timeStyle: 'medium',
+				},
+			},
+			//自定义上下页名
+			docFooter: {
+				prev: '上一页',
+				next: '下一页',
+			},
+			//markdown配置
+			markdown: {
+				image: {
+					// 开启图片懒加载
+					lazyLoading: true,
+				},
+				//时间线
+				config: md => {
+					md.use(timeline)
+				},
+			},
+			socialLinks: [{ icon: 'github', link: 'https://github.com/xarzhi/xarzhi.github.io' }],
+			footer: {
+				message: 'MIT Licensed',
+				copyright: 'Copyright © 2024-present xarzhi',
 			},
 		},
-		//自定义上下页名
-		docFooter: {
-			prev: '上一页',
-			next: '下一页',
-		},
-		//markdown配置
-		markdown: {
-			image: {
-				// 开启图片懒加载
-				lazyLoading: true,
-			},
-			//时间线
-			config: md => {
-				md.use(timeline)
-			},
-		},
-		socialLinks: [{ icon: 'github', link: 'https://github.com/xarzhi/xarzhi.github.io' }],
-		footer: {
-			message: 'MIT Licensed',
-			copyright: 'Copyright © 2024-present xarzhi',
-		},
-	},
-})
+	})
+)

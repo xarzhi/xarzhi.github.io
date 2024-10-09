39d540ee1fbd3d7a15dec5f5136f005d8ac035fb
tyhang <xarzhi@163.com>
2024-10-09 16:02:40 +0800

    up

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -98,24 +98,6 @@ export default withPwa(
 				short_name: 'xarzhi', // 应用的短名称
 				description: '个人笔记', // 应用的描述
 				theme_color: '#ffffff', // 主题颜色
-				icons: [
-					{
-						src: '/images/pwa-120x120.png', // 图标路径
-						sizes: '120x120', // 图标尺寸
-						type: 'image/png', // 图标类型
-					},
-					{
-						src: '/images/pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-					{
-						src: '/images/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any',
-					},
-				],
 			},
 			workbox: {
 				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'], // 匹配需要缓存的文件类型

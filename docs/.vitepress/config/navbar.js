5dc42d28118587a6f7b9ca013d585e0b0838303e
tyhang <xarzhi@163.com>
2025-01-06 16:21:16 +0800

    up

--- docs/.vitepress/config/navbar.js
+++ docs/.vitepress/config/navbar.js
@@ -1,106 +1,128 @@
 export default [
-	{
-		text: '前端',
-		items: [
-			{
-				text: '前端三剑客',
-				items: [
-					{
-						text: '🍉Html',
-						link: '/docs/前端/前端三剑客/Html/01.Html初识',
-					},
-					{
-						text: '🍊CSS',
-						link: '/docs/前端/前端三剑客/CSS/CSS/01.引入css样式表',
-					},
-					{
-						text: '🍇JavaScript',
-						link: '/docs/前端/前端三剑客/javascript/js基础/01.js基础语法',
-					},
-				],
-			},
-			{
-				text: 'TypeScript',
-				items: [{ text: '🚢Ts入门', link: '/docs/前端/TypeScript/Ts入门/01.基本配置' }],
-			},
-			{
-				text: 'Vue',
-				items: [
-					{ text: '🥈Vue2', link: '/docs/前端/框架/Vue/Vue2/Vue2基础/01.Vue2基础' },
-					{ text: '🥉Vue3', link: '/docs/前端/框架/Vue/Vue3/Vue3基础/01.Vue3基础' },
-				],
-			},
-			{
-				text: 'React',
-				items: [{ text: '🐨React', link: '/docs/前端/框架/React/01.React入门' }],
-			},
-			{
-				text: '可视化',
-				items: [
-					{ text: '🍬Canvas', link: '/docs/前端/可视化/Canvas/01.认识Canvas' },
-					{ text: '🍭WebGl', link: '/docs/前端/可视化/WebGL/01.WebGL基础' },
-					{ text: '🍦Threejs', link: '/docs/前端/可视化/Three.js/基本使用/01.安装threejs' },
-				],
-			},
-			{
-				text: '第三方库',
-				items: [{ text: '💤Gsap', link: '/docs/前端/第三方库/Gsap/01.基本使用' }],
-			},
-			{
-				text: '🛀小程序',
-				link: '/docs/前端/小程序/01.mina.md',
-			},
-			{
-				text: '⛵Webpack',
-				link: '/docs/前端/Webpack/01.Webpack基础.md',
-			},
-			{
-				text: '🧭一些问题',
-				link: '/docs/前端/一些问题/01.后端返回10万条数据.md',
-			},
-		],
-	},
-	{
-		text: '后端',
-		items: [
-			{
-				text: 'Node.js',
-				link: '/docs/后端/Node.js/01.初识Node.js',
-			},
-			{
-				text: 'C++',
-				items: [
-					{
-						text: '🍌C++基础',
-						link: '/docs/后端/C++/C++基础/C++基础/01.环境配置.md',
-					},
-					{
-						text: '🍍Win32 api',
-						link: '/docs/后端/C++/Win32/01.基础知识.md',
-					},
-					{
-						text: '🍉QT',
-						link: '/docs/后端/C++/C++基础/01.环境配置.md',
-					},
-				],
-			},
-			{
-				text: 'C#',
-				items: [
-					{
-						text: '🍌C#基础',
-						link: '/docs/后端/CS/CS基础/CS基础/01.环境配置.md',
-					},
-					{
-						text: '🍌WPF',
-						link: '/docs/后端/CS/WPF/01.环境配置.md',
-					},
-				],
-			},
-		],
-	},
-	{
-		text: '学习git',
-		link: '/docs/学习git/git',
-	},
-]
+  {
+    text: "前端",
+    items: [
+      {
+        text: "前端三剑客",
+        items: [
+          {
+            text: "🍉Html",
+            link: "/docs/前端/前端三剑客/Html/01.Html初识",
+          },
+          {
+            text: "🍊CSS",
+            link: "/docs/前端/前端三剑客/CSS/CSS/01.引入css样式表",
+          },
+          {
+            text: "🍇JavaScript",
+            link: "/docs/前端/前端三剑客/javascript/js基础/01.js基础语法",
+          },
+        ],
+      },
+      {
+        text: "TypeScript",
+        items: [
+          {
+            text: "🚢Ts入门",
+            link: "/docs/前端/TypeScript/Ts入门/01.基本配置",
+          },
+        ],
+      },
+      {
+        text: "Vue",
+        items: [
+          {
+            text: "🥈Vue2",
+            link: "/docs/前端/框架/Vue/Vue2/Vue2基础/01.Vue2基础",
+          },
+          {
+            text: "🥉Vue3",
+            link: "/docs/前端/框架/Vue/Vue3/Vue3基础/01.Vue3基础",
+          },
+        ],
+      },
+      {
+        text: "React",
+        items: [
+          { text: "🐨React", link: "/docs/前端/框架/React/01.React入门" },
+        ],
+      },
+      {
+        text: "可视化",
+        items: [
+          { text: "🍬Canvas", link: "/docs/前端/可视化/Canvas/01.认识Canvas" },
+          { text: "🍭WebGl", link: "/docs/前端/可视化/WebGL/01.WebGL基础" },
+          {
+            text: "🍦Threejs",
+            link: "/docs/前端/可视化/Three.js/基本使用/01.安装threejs",
+          },
+        ],
+      },
+      {
+        text: "第三方库",
+        items: [
+          { text: "💤Gsap", link: "/docs/前端/第三方库/Gsap/01.基本使用" },
+        ],
+      },
+      {
+        text: "🛀小程序",
+        link: "/docs/前端/小程序/01.mina.md",
+      },
+      {
+        text: "⛵Webpack",
+        link: "/docs/前端/Webpack/01.Webpack基础.md",
+      },
+      {
+        text: "🧭一些问题",
+        link: "/docs/前端/一些问题/01.后端返回10万条数据.md",
+      },
+    ],
+  },
+  {
+    text: "后端",
+    items: [
+      {
+        text: "Node.js",
+        link: "/docs/后端/Node.js/01.初识Node.js",
+      },
+      {
+        text: "C++",
+        items: [
+          {
+            text: "🍌C++基础",
+            link: "/docs/后端/C++/C++基础/C++基础/01.环境配置.md",
+          },
+          {
+            text: "🍍Win32 api",
+            link: "/docs/后端/C++/Win32/01.基础知识.md",
+          },
+          {
+            text: "🍉QT",
+            link: "/docs/后端/C++/C++基础/01.环境配置.md",
+          },
+        ],
+      },
+      {
+        text: "C#",
+        items: [
+          {
+            text: "🍌C#基础",
+            link: "/docs/后端/CS/CS基础/CS基础/01.环境配置.md",
+          },
+          {
+            text: "🍌WPF",
+            link: "/docs/后端/CS/WPF/01.环境配置.md",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "学习git",
+    link: "/docs/学习git/git",
+  },
+  {
+    text: "其他问题",
+    link: "/docs/其他问题/01.Typora使用PicGo实现图床",
+  },
+];
--- docs/.vitepress/config/sidebar.js
+++ docs/.vitepress/config/sidebar.js
@@ -1,99 +1,187 @@
-import { join } from 'path'
-import { readdirSync } from 'fs'
+import { join } from "path";
+import { readdirSync } from "fs";
 
 function isMarkdownFile(fileName) {
-	return !!fileName.match(/.+\.md$/)
+  return !!fileName.match(/.+\.md$/);
 }
 function getItems(url) {
-	const path = join(__dirname, `../../${url}`)
-	return readdirSync(path)
-		.filter(item => isMarkdownFile(item))
-		.map(fileName => {
-			if (isMarkdownFile(fileName)) {
-				const text = fileName.match(/^[0-9]{2}(-|.)+/) ? fileName.substring(3) : fileName
-				const fileData = {
-					text: text.replace('.md', ''),
-					link: `${url}/${fileName}`,
-				}
-				return fileData
-			}
-		})
+  const path = join(__dirname, `../../${url}`);
+  return readdirSync(path)
+    .filter((item) => isMarkdownFile(item))
+    .map((fileName) => {
+      if (isMarkdownFile(fileName)) {
+        const text = fileName.match(/^[0-9]{2}(-|.)+/)
+          ? fileName.substring(3)
+          : fileName;
+        const fileData = {
+          text: text.replace(".md", ""),
+          link: `${url}/${fileName}`,
+        };
+        return fileData;
+      }
+    });
 }
 
 const sidebar = {
-	'/docs/前端/前端三剑客/Html/': getItems('docs/前端/前端三剑客/Html'),
-	'/docs/前端/前端三剑客/CSS': [
-		{ text: 'CSS', items: getItems('/docs/前端/前端三剑客/CSS/CSS') },
-		{ text: 'CSS3', items: getItems('/docs/前端/前端三剑客/CSS/CSS3') },
-	],
-	'/docs/前端/前端三剑客/javascript/': [
-		{ text: 'js基础', items: getItems('/docs/前端/前端三剑客/javascript/js基础') },
-		{ text: 'js高级', items: getItems('/docs/前端/前端三剑客/javascript/js高级') },
-		{ text: 'Web Api', items: getItems('/docs/前端/前端三剑客/javascript/WebApi') },
-	],
-	'/docs/前端/第三方库/': [
-		{
-			text: 'Gsap',
-			items: getItems('/docs/前端/第三方库/Gsap'),
-		},
-	],
-	'/docs/前端/框架/Vue/Vue2/': [
-		{ text: 'Vue2基础', items: getItems('/docs/前端/框架/Vue/Vue2/Vue2基础') },
-		{ text: 'Vue2进阶', items: getItems('/docs/前端/框架/Vue/Vue2/Vue2进阶') },
-	],
-	'/docs/前端/框架/Vue/Vue3/': [
-		{ text: 'Vue3基础', items: getItems('/docs/前端/框架/Vue/Vue3/Vue3基础') },
-		{ text: '组合式Api', items: getItems('/docs/前端/框架/Vue/Vue3/组合式Api') },
-		{ text: 'Vue3进阶', items: getItems('/docs/前端/框架/Vue/Vue3/Vue3进阶'), collapsed: true },
-		{ text: '内置组件', items: getItems('/docs/前端/框架/Vue/Vue3/内置组件'), collapsed: true },
-	],
-	'/docs/前端/TypeScript/Ts入门/': [{ text: 'Ts入门', items: getItems('/docs/前端/TypeScript/Ts入门') }],
-	'/docs/前端/框架/React/': getItems('/docs/前端/框架/React'),
-	'/docs/前端/可视化/Canvas/': getItems('/docs/前端/可视化/Canvas'),
-	'/docs/前端/可视化/WebGL/': getItems('/docs/前端/可视化/WebGL'),
-	'/docs/前端/可视化/Three.js/': [
-		{ text: '基本使用', items: getItems('/docs/前端/可视化/Three.js/基本使用') },
-		{ text: '几何体', items: getItems('/docs/前端/可视化/Three.js/几何体'), collapsed: true },
-		{ text: '材质', items: getItems('/docs/前端/可视化/Three.js/材质'), collapsed: true },
-		{ text: '物体', items: getItems('/docs/前端/可视化/Three.js/物体'), collapsed: true },
-		{ text: '纹理贴图', items: getItems('/docs/前端/可视化/Three.js/纹理贴图'), collapsed: true },
-		{ text: '光源', items: getItems('/docs/前端/可视化/Three.js/光源'), collapsed: true },
-		{ text: '阴影', items: getItems('/docs/前端/可视化/Three.js/阴影'), collapsed: true },
-		{ text: '场景', items: getItems('/docs/前端/可视化/Three.js/场景'), collapsed: true },
-		{ text: '控制器', items: getItems('/docs/前端/可视化/Three.js/控制器'), collapsed: true },
-		{ text: '辅助对象', items: getItems('/docs/前端/可视化/Three.js/辅助对象'), collapsed: true },
-		{ text: '摄像机', items: getItems('/docs/前端/可视化/Three.js/摄像机'), collapsed: true },
-		{ text: '加载器', items: getItems('/docs/前端/可视化/Three.js/加载器'), collapsed: true },
-		{ text: '核心', items: getItems('/docs/前端/可视化/Three.js/核心'), collapsed: true },
-		{ text: '数学库', items: getItems('/docs/前端/可视化/Three.js/数学库'), collapsed: true },
-		{ text: '常量', items: getItems('/docs/前端/可视化/Three.js/常量'), collapsed: true },
-	],
-	'/docs/后端/C++/C++基础': [
-		{ text: 'C++基础', items: getItems('/docs/后端/C++/C++基础/C++基础') },
-		{ text: 'C++进阶', items: getItems('/docs/后端/C++/C++基础/C++进阶') },
-		{ text: '面向对象', items: getItems('/docs/后端/C++/C++基础/面向对象') },
-		{ text: 'STL', items: getItems('/docs/后端/C++/C++基础/STL') },
-	],
-	'/docs/后端/CS/CS基础': [
-		{ text: 'CS基础', items: getItems('/docs/后端/CS/CS基础/CS基础') },
-		{ text: 'CS高级', items: getItems('/docs/后端/CS/CS基础/CS高级') },
-		{ text: '面向对象', items: getItems('/docs/后端/CS/CS基础/面向对象') },
-	],
-	'/docs/后端/CS/WPF': [{ text: 'WPF基础', items: getItems('/docs/后端/CS/WPF') }],
-	'/docs/后端/C++/Win32': [{ text: 'Win32', items: getItems('/docs/后端/C++/Win32') }],
-	'/docs/前端/小程序/': getItems('/docs/前端/小程序'),
-	'/docs/后端/Node.js/': getItems('/docs/后端/Node.js/'),
-	'/docs/前端/一些问题/': getItems('/docs/前端/一些问题/'),
-}
+  "/docs/前端/前端三剑客/Html/": getItems("docs/前端/前端三剑客/Html"),
+  "/docs/前端/前端三剑客/CSS": [
+    { text: "CSS", items: getItems("/docs/前端/前端三剑客/CSS/CSS") },
+    { text: "CSS3", items: getItems("/docs/前端/前端三剑客/CSS/CSS3") },
+  ],
+  "/docs/前端/前端三剑客/javascript/": [
+    {
+      text: "js基础",
+      items: getItems("/docs/前端/前端三剑客/javascript/js基础"),
+    },
+    {
+      text: "js高级",
+      items: getItems("/docs/前端/前端三剑客/javascript/js高级"),
+    },
+    {
+      text: "Web Api",
+      items: getItems("/docs/前端/前端三剑客/javascript/WebApi"),
+    },
+  ],
+  "/docs/前端/第三方库/": [
+    {
+      text: "Gsap",
+      items: getItems("/docs/前端/第三方库/Gsap"),
+    },
+  ],
+  "/docs/前端/框架/Vue/Vue2/": [
+    { text: "Vue2基础", items: getItems("/docs/前端/框架/Vue/Vue2/Vue2基础") },
+    { text: "Vue2进阶", items: getItems("/docs/前端/框架/Vue/Vue2/Vue2进阶") },
+  ],
+  "/docs/前端/框架/Vue/Vue3/": [
+    { text: "Vue3基础", items: getItems("/docs/前端/框架/Vue/Vue3/Vue3基础") },
+    {
+      text: "组合式Api",
+      items: getItems("/docs/前端/框架/Vue/Vue3/组合式Api"),
+    },
+    {
+      text: "Vue3进阶",
+      items: getItems("/docs/前端/框架/Vue/Vue3/Vue3进阶"),
+      collapsed: true,
+    },
+    {
+      text: "内置组件",
+      items: getItems("/docs/前端/框架/Vue/Vue3/内置组件"),
+      collapsed: true,
+    },
+  ],
+  "/docs/前端/TypeScript/Ts入门/": [
+    { text: "Ts入门", items: getItems("/docs/前端/TypeScript/Ts入门") },
+  ],
+  "/docs/前端/框架/React/": getItems("/docs/前端/框架/React"),
+  "/docs/前端/可视化/Canvas/": getItems("/docs/前端/可视化/Canvas"),
+  "/docs/前端/可视化/WebGL/": getItems("/docs/前端/可视化/WebGL"),
+  "/docs/前端/可视化/Three.js/": [
+    {
+      text: "基本使用",
+      items: getItems("/docs/前端/可视化/Three.js/基本使用"),
+    },
+    {
+      text: "几何体",
+      items: getItems("/docs/前端/可视化/Three.js/几何体"),
+      collapsed: true,
+    },
+    {
+      text: "材质",
+      items: getItems("/docs/前端/可视化/Three.js/材质"),
+      collapsed: true,
+    },
+    {
+      text: "物体",
+      items: getItems("/docs/前端/可视化/Three.js/物体"),
+      collapsed: true,
+    },
+    {
+      text: "纹理贴图",
+      items: getItems("/docs/前端/可视化/Three.js/纹理贴图"),
+      collapsed: true,
+    },
+    {
+      text: "光源",
+      items: getItems("/docs/前端/可视化/Three.js/光源"),
+      collapsed: true,
+    },
+    {
+      text: "阴影",
+      items: getItems("/docs/前端/可视化/Three.js/阴影"),
+      collapsed: true,
+    },
+    {
+      text: "场景",
+      items: getItems("/docs/前端/可视化/Three.js/场景"),
+      collapsed: true,
+    },
+    {
+      text: "控制器",
+      items: getItems("/docs/前端/可视化/Three.js/控制器"),
+      collapsed: true,
+    },
+    {
+      text: "辅助对象",
+      items: getItems("/docs/前端/可视化/Three.js/辅助对象"),
+      collapsed: true,
+    },
+    {
+      text: "摄像机",
+      items: getItems("/docs/前端/可视化/Three.js/摄像机"),
+      collapsed: true,
+    },
+    {
+      text: "加载器",
+      items: getItems("/docs/前端/可视化/Three.js/加载器"),
+      collapsed: true,
+    },
+    {
+      text: "核心",
+      items: getItems("/docs/前端/可视化/Three.js/核心"),
+      collapsed: true,
+    },
+    {
+      text: "数学库",
+      items: getItems("/docs/前端/可视化/Three.js/数学库"),
+      collapsed: true,
+    },
+    {
+      text: "常量",
+      items: getItems("/docs/前端/可视化/Three.js/常量"),
+      collapsed: true,
+    },
+  ],
+  "/docs/后端/C++/C++基础": [
+    { text: "C++基础", items: getItems("/docs/后端/C++/C++基础/C++基础") },
+    { text: "C++进阶", items: getItems("/docs/后端/C++/C++基础/C++进阶") },
+    { text: "面向对象", items: getItems("/docs/后端/C++/C++基础/面向对象") },
+    { text: "STL", items: getItems("/docs/后端/C++/C++基础/STL") },
+  ],
+  "/docs/后端/CS/CS基础": [
+    { text: "CS基础", items: getItems("/docs/后端/CS/CS基础/CS基础") },
+    { text: "CS高级", items: getItems("/docs/后端/CS/CS基础/CS高级") },
+    { text: "面向对象", items: getItems("/docs/后端/CS/CS基础/面向对象") },
+  ],
+  "/docs/后端/CS/WPF": [
+    { text: "WPF基础", items: getItems("/docs/后端/CS/WPF") },
+  ],
+  "/docs/后端/C++/Win32": [
+    { text: "Win32", items: getItems("/docs/后端/C++/Win32") },
+  ],
+  "/docs/前端/小程序/": getItems("/docs/前端/小程序"),
+  "/docs/后端/Node.js/": getItems("/docs/后端/Node.js/"),
+  "/docs/前端/一些问题/": getItems("/docs/前端/一些问题/"),
+  "/docs/其他问题": getItems("/docs/其他问题"),
+};
 
 for (let key in sidebar) {
-	if (Array.isArray(sidebar[key])) {
-		sidebar[key] = sidebar[key].map(item => {
-			return {
-				...item,
-				collapsed: item.collapsed || false,
-			}
-		})
-	}
+  if (Array.isArray(sidebar[key])) {
+    sidebar[key] = sidebar[key].map((item) => {
+      return {
+        ...item,
+        collapsed: item.collapsed || false,
+      };
+    });
+  }
 }
-export default sidebar
+export default sidebar;
